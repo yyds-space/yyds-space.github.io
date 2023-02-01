1f81919dd5b5df758d7ba979eb4ff61de3ef010a
whb <17614902580@163.com>
2023-02-01 16:24:23 +0800

    Site updated: 2023-02-01 16:24:23

---
+++ js/tanchuang.js
@@ -0,0 +1,40 @@
+new Vue({
+    data: function () {
+        this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+        });
+    }
+})
+
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
