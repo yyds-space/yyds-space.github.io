773e72b91816a603df287fbd72d8ee07fdbd82fa
whb <17614902580@163.com>
2023-02-01 21:57:11 +0800

    Site updated: 2023-02-01 21:57:11

--- js/txmap.js
+++ js/txmap.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(40.06452971873027, 116.21574808571324, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
@@ -64,7 +64,7 @@ function showWelcome() {
             ip = ipLoacation.result.ip;
             switch (ipLoacation.result.ad_info.province) {
                 case "北京市":
-                    posdesc = "北——京——欢迎你~~~";
+                    posdesc = "北—京—欢迎你~~~";
                     break;
                 case "天津市":
                     posdesc = "讲段相声吧。";
