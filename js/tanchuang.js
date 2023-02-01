c91d282e8d7758c31e591ad25088a68dcc9bdb7e
whb <17614902580@163.com>
2023-02-01 17:01:50 +0800

    Site updated: 2023-02-01 17:01:50

--- js/tanchuang.js
+++ js/tanchuang.js
@@ -1,16 +1,16 @@
-new Vue({
-    data: function () {
-        this.$notify({
-            title: "你已被发现😜",
-            message: "小伙子，扒源记住要遵循GPL协议！",
-            position: 'top-left',
-            offset: 50,
-            showClose: true,
-            type: "warning",
-            duration: 5000
-        });
-    }
-})
+// new Vue({
+//     data: function () {
+//         this.$notify({
+//             title: "你已被发现😜",
+//             message: "小伙子，扒源记住要遵循GPL协议！",
+//             position: 'top-left',
+//             offset: 50,
+//             showClose: true,
+//             type: "warning",
+//             duration: 5000
+//         });
+//     }
+// })
 
 // 防抖全局计时器
 let TT = null;    //time用来控制事件的触发
